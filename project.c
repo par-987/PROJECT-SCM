ccdaa980bc859b1c8ec0250dd930a7c8ad022bf1
prabhjot <prabhjot@gmail.com>
2023-03-17 12:15:21 +0530

    formula to calculate 1st day of week

--- project.c
+++ project.c
@@ -1,2 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+int get_1st_Weekday(int year){
+    int d;
+    d = (((year - 1) * 365) + ((year - 1) / 4) - ((year - 1) / 100) + ((year) / 400) + 1) % 7;
+  return d;
+}
